7f6246beebe85cf8f8529731c3f8305a4f527b37
zhanglintc <zhanglintc623@gmail.com>
2025-04-27 16:41:45 +0800

    pom with modules

---
+++ springBootMQ/src/main/java/co/zhanglintc/ohMySpringBoot/SpringbootMQ.java
@@ -0,0 +1,12 @@
+package co.zhanglintc.ohMySpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMQ {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMQ.class, args);
+    }
+
+}
